ed27d9e87cb5df0f165093ac011a9cfe3887c4be
Mehmet <mehmetbilgili@posta.mu.edu.tr>
2024-05-06 05:48:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,27 +8,21 @@ const io = socketIo(server);
 
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static('public'));
 
-// WebSocket (Socket.IO) server logic
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  // Listen for chat message events from clients
   socket.on('chat message', (data) => {
     console.log('Message received:', data);
-    // Broadcast the message to all connected clients
     io.emit('chat message', data);
   });
 
-  // Handle disconnection
   socket.on('disconnect', () => {
     console.log('A user disconnected:', socket.id);
   });
 });
 
-// Start the HTTP server
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
